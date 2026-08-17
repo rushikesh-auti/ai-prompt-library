bdb1ad02b11d46f7e5192bef8e4a24fd14953657
Rushikesh Auti <autirushikesh758@gmail.com>
2026-08-17 22:23:33 +0530

    Connect MongoDB database

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-// Health check
 app.get("/", (req, res) => {
   res.json({
     success: true,
